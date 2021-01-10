6d8c45690c47821e37ce3a17eb613003c49a4bcc
BURAK SENTURK <burak.senturk@isik.edu.tr>
2021-01-10 18:33:14 +0300

    createsurvey

---
+++ client/express/js/core/createSurvey.js
@@ -0,0 +1,33 @@
+$(document).ready(() => {
+    var dateFormat = "mm/dd/yy",
+        from = $("#startS")
+        .datepicker({
+            defaultDate: "+1w",
+            changeMonth: true,
+            numberOfMonths: 1
+        })
+        .on("change", function() {
+            to.datepicker("option", "minDate", getDate(this));
+        }),
+        to = $("#endS").datepicker({
+            defaultDate: "+1w",
+            changeMonth: true,
+            numberOfMonths: 1
+        })
+        .on("change", function() {
+            from.datepicker("option", "maxDate", getDate(this));
+        });
+
+    function getDate(element) {
+        var date;
+        try {
+            date = $.datepicker.parseDate(dateFormat, element.value);
+        } catch (error) {
+            date = null;
+        }
+
+        return date;
+    }
+}
+
+);
---
+++ client/express/js/question.js
@@ -0,0 +1,44 @@
+
+
+const btnAdd = document.querySelector('#btnAdd');
+        const btnRemove = document.querySelector('#btnRemove');
+        const sb = document.querySelector('#list');
+        const name = document.querySelector('#name');
+
+        btnAdd.onclick = (e) => {
+            e.preventDefault();
+
+            // validate the option
+            if (name.value == '') {
+                alert('Please enter the name.');
+                return;
+            }
+            // create a new option
+            const option = new Option(name.value, name.value);
+            // add it to the list
+            sb.add(option, undefined);
+
+            // reset the value of the input
+            name.value = '';
+            name.focus();
+        };
+
+        // remove selected option
+        btnRemove.onclick = (e) => {
+            e.preventDefault();
+
+            // save the selected option
+            let selected = [];
+
+            for (let i = 0; i < sb.options.length; i++) {
+                selected[i] = sb.options[i].selected;
+            }
+
+            // remove all selected option
+            let index = sb.options.length;
+            while (index--) {
+                if (selected[index]) {
+                    sb.remove(index);
+                }
+            }
+        };
