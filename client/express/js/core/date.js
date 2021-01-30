a8b98c1c0c09d627959f2081a2cd1d98bcd09ba6
doga <dogaozyagci@icloud.com>
2021-01-30 21:36:38 +0300

    yaş sınırlaması ( 18+ )

---
+++ client/express/js/core/date.js
@@ -0,0 +1,14 @@
+$(function(){
+    var dtToday = new Date();
+
+    var month = dtToday.getMonth() + 1;// jan=0; feb=1 .......
+    var day = dtToday.getDate();
+    var year = dtToday.getFullYear() - 18;
+    if(month < 10)
+        month = '0' + month.toString();
+    if(day < 10)
+        day = '0' + day.toString();
+    var minDate = year + '-' + month + '-' + day;
+    var maxDate = year + '-' + month + '-' + day;
+    $('#dob').attr('max', maxDate);
+});
