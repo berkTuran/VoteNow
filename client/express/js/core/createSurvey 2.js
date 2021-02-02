3bb788efbd6c67f445feb2527fcf566f724fc46b
doga <dogaozyagci@icloud.com>
2021-02-02 12:06:31 +0300

    last edits

--- client/express/js/core/createSurvey 2.js
+++ client/express/js/core/createSurvey 2.js
@@ -1,4 +1,11 @@
 $(document).ready(() => {
+    if (localStorage.getItem("id") == null) {
+        location.href = "/signin.html";
+        alert("You need to login")
+        
+    }
+    let answers = [];
+    
     var dateFormat = "mm/dd/yy",
         from = $("#startS")
             .datepicker({
@@ -28,6 +35,49 @@ $(document).ready(() => {
 
         return date;
     }
+
+    $("#btnAdd").on("click", () => {
+        if ($("#answer").val() !== "" ) {
+            answers.push({
+                answerName: $("#answer").val(),
+               
+            });
+
+            $("#answer").val(null);
+        }
+    });
+
+
+    $("#createSurveynBtn").on("click", async () => {
+        if (answers.length > 0) {
+            let model = {};
+            model.surveyName = $("#surveyName").val();
+            model.surveyDiscription = $("#surveyDesc").val();
+            model.startDate = $("#start").val();
+            model.endDate = $("#end").val();
+            model.answers = [];
+            model.capacity = 100;
+            console.log(model);
+
+            for (let i of answers) {
+                model.answers.push({
+                    answerName: i.answerName,
+                });
+            }
+            console.log(model);
+
+            let result = await $.post("https://us-central1-votenow-e5dc8.cloudfunctions.net/createSurvey", model);
+            console.log(result);
+            if (!result.error)
+                alert(result.error.message);
+            else {
+                alert("Survey create was successful!");
+                location.reload();
+            }
+        } else {
+            alert("First you need to add surveys.");
+        }
+    });
 }
 
 );
--- client/express/js/core/finishedElections copy.js
+++ client/express/js/core/finishedElections copy.js
@@ -2,15 +2,15 @@ $(document).ready(() => {
     var modal = {}
     modal.status=false
     
-    $.post("https://us-central1-votenow-e5dc8.cloudfunctions.net/getAllElections", modal).then(e => {
+    $.post("https://us-central1-votenow-e5dc8.cloudfunctions.net/getAllSurveys", modal).then(e => {
         let row = $("#finished");
         
         for (let i of e.result) {
           console.log(e.result);  
             row.append(`<div class="boxed boxed--border bg--secondary boxed--lg box-shadow mx-auto" style='overflow-wrap: break-word;'>
                  
-                <h5>${i.data.electionName}</h5>
-                <div id='vote-now'><a class="btn btn--primary" data-field='${i.id}' href="result.html" ><span class="btn__text" data-field='${i.id}'>Results</span></a></div></div>`
+                <h5>${i.data.surveyName}</h5>
+                <div id='vote-now'><a class="btn btn--primary" data-field='${i.id}' href="surveyresult.html" ><span class="btn__text" data-field='${i.id}'>Results</span></a></div></div>`
         );
         }
     });
