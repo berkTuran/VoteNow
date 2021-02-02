039db4dacd8d4a7a0343a4ada29757e25f2f19d9
Berk <berk.turan2@isik.edu.tr>
2021-02-03 02:24:46 +0300

    Result page improvement

--- client/express/js/core/finishedElections.js
+++ client/express/js/core/finishedElections.js
@@ -9,7 +9,7 @@ $(document).ready(() => {
           console.log(e.result);  
             row.append(`<div class="boxed boxed--border bg--secondary boxed--lg box-shadow mx-auto" style='overflow-wrap: break-word;'>
                  
-                <h5>${i.data.electionName}</h5>
+                <h5>${i.electionName}</h5>
                 <div id='vote-now'><a class="btn btn--primary" data-field='${i.id}' href="result.html" ><span class="btn__text" data-field='${i.id}'>Results</span></a></div></div>`
         );
         }
