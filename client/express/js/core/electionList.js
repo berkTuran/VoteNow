e33fec63c23f8e78a09c12cada833815e405ddf6
canpeksoz <canpeksoz@gmail.com>
2021-02-03 05:12:35 +0300

    vote sayfası buton hariç hazır

--- client/express/js/core/electionList.js
+++ client/express/js/core/electionList.js
@@ -4,13 +4,13 @@ $(document).ready(() => {
     modal.status = true;
 
     $.post("https://us-central1-votenow-e5dc8.cloudfunctions.net/getAllElections", modal).then(e => {
-        console.log(e.result);
+
         for (let i of e.result) {
             let row = $("#election-list");
             row.append(`<div class="boxed boxed--border bg--secondary boxed--lg box-shadow mx-auto" style='overflow-wrap: break-word;'>
             <h5>${i.electionName}</h5><p>${i.electionDiscription}</p>
             
-            <div id='vote-now'><a class="btn btn--primary" data-field='${i.id}'><span class="btn__text" data-field='${i.id}'>Vote Now</span></a></div></div>`);
+            <div id='vote-now'><a class="btn btn--primary" data-field='${i.electionId}'><span class="btn__text" data-field='${i.electionId}'>Vote Now</span></a></div></div>`);
         }
     });
 
--- client/express/js/core/vote.js
+++ client/express/js/core/vote.js
@@ -1,7 +1,61 @@
 $(document).ready(() => {
-    let model = { electionId: localStorage.getItem("vote-election-id") };
-    $.get("https://us-central1-votenow-e5dc8.cloudfunctions.net/getElection", model)
+
+
+    let model = {};
+    model.status = true;
+
+    $.post("https://us-central1-votenow-e5dc8.cloudfunctions.net/getAllElections", model)
         .then(e => {
-            $("div.row div.col-md-10.col-lg-8")
+
+            var election = {};
+
+            let electionId = localStorage.getItem("vote-election-id");
+            e.result.forEach(element => {
+
+                if (element.electionId == electionId) {
+                    election = element;
+                    //break;
+
+                }
+
+            });
+            console.log(election);
+
+
+           
+
+            
+            election.candidates.forEach(candidate => {
+                console.log(candidate);
+                let row = $("#candidate-list");
+                row.append(`<div class="boxed boxed--border bg--secondary boxed--lg box-shadow mx-auto" style='overflow-wrap: break-word;'>
+            
+            <img src="${candidate.profileImageUrl}"  width="200" height="200"> </img>
+            <h2>${candidate.firstName+" "+candidate.lastName}</h2>
+            <p>${candidate.bio}</p> 
+            
+            
+
+            
+            <div id='vote-now'><a class="btn btn--primary" data-field='${candidate.firstName}'><span class="btn__text" data-field='${candidate.lastName}'>Vote Now</span></a></div></div>`);
+        
+
+            });
+
+
+
+
+
+
+
+
+            
+            //$("div.row div.col-md-10.col-lg-8")
         });
 });
+
+
+
+
+
+
