b36aa771022076b9f23d38de49a713207bf66d18
Berk <berk.turan2@isik.edu.tr>
2021-02-03 04:09:44 +0300

    updateElectionStatus function is added

--- api/functions/index.js
+++ api/functions/index.js
@@ -430,3 +430,36 @@ exports.voteElection = functions.https.onRequest(async (req, res) => {
 
     });
 });
+
+exports.updateElectionStatus = functions.https.onRequest(async (req, res) => {
+    let electionsRef = admin.firestore().collection('elections')
+    electionsRef.get().then(snapshot => {
+        let promises = []
+        snapshot.forEach(document => {
+            let election = document.data()
+            let endDate = stringToDate(election.endDate, "mm/dd/yyyy","/")
+            let today = new Date()
+            if (today>endDate) {
+                let promise = electionsRef.doc(document.id).update({isOpen: false})
+                promises.push(promise)
+            }
+        });
+        Promise.all(promises).then(responses => {
+            res.json(responses)
+        })
+    });
+});
+
+function stringToDate(_date,_format,_delimiter)
+{
+            var formatLowerCase=_format.toLowerCase();
+            var formatItems=formatLowerCase.split(_delimiter);
+            var dateItems=_date.split(_delimiter);
+            var monthIndex=formatItems.indexOf("mm");
+            var dayIndex=formatItems.indexOf("dd");
+            var yearIndex=formatItems.indexOf("yyyy");
+            var month=parseInt(dateItems[monthIndex]);
+            month-=1;
+            var formatedDate = new Date(dateItems[yearIndex],month,dateItems[dayIndex]);
+            return formatedDate;
+}
